97860735314c36a9633a0a1872fc14083a594b17
Bruce-Young <ydl92@qq.com>
2014-07-06 23:34:32 +0800

    删除测试源码，整理ShortPathSegment源码模块

--- test/SegFileTest.cpp
+++ test/SegFileTest.cpp
@@ -1,5 +1,5 @@
 
-#include "../Trie.hpp"
+#include "../ShortPathSegment.hpp"
 #include <iostream>
 
 using namespace std;
@@ -14,23 +14,23 @@ int main(int argc, char* argv[])
 	    return 0;
 	}
 
-	ShortPathSegment trie;
+	ShortPathSegment segMan;
 	time_t time_s, time_e;
 	time_s = clock();
-	trie.loadDict(argv[1]);
+	segMan.loadDict(argv[1]);
 	time_e = clock();
 	cout << " success in loadDict in " 
          << (double)(time_e - time_s)/CLOCKS_PER_SEC << endl;
-	cout << " insert " << trie.getSize() << " words" << endl;
+	cout << " insert " << segMan.getSize() << " words" << endl;
 
     vector<string> outString;
 	time_s = clock();
-    trie.matchTextFile(argv[2], outString, "|");
+    segMan.matchTextFile(argv[2], outString, "|");
     time_e = clock();
     cout << " success in matchFile in " 
          << (double)(time_e - time_s)/CLOCKS_PER_SEC << endl;
    
-    trie.OutString2File(outString, "out.txt");
+    segMan.OutString2File(outString, "out.txt");
     cout << "enter to quit!" << endl;
     getchar();
 	return 0;
